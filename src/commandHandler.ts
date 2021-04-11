dc213cba4c99aab140d707b3c822f2bbd02a1bd4
Krzysztof Kowalczyk <kowalczykkrzysztof893@gmail.com>
2021-04-11 23:36:26 +0200

    Added types

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -1,31 +1,32 @@
+// Dotenv
 import dotenv from 'dotenv';
+// Discord
 import { Message } from 'discord.js';
 // Commands
 import { tenor } from './commands/tenor';
-
+// Dotenv config
 dotenv.config({ path: 'config.env' });
 // Command prefix
 const prefix: string = process.env.PREFIX as string;
-
-// List of commands, type has to be any unfortunately
-const commands: any = {
+// List of commands
+const commands: {
+  [key: string]: (msg: Message, ...args: string[]) => Promise<Message>;
+} = {
   tenor,
 };
-
+// Command handler
 export const commandHandler = (msg: Message) => {
   // If msg doesn't start with prefix or author is bot return
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
   // msg.content is a string, slice the prefix then remove whitespace
-  const content: string[] | number[] = msg.content
-    .slice(prefix.length)
-    .split(/ +/);
+  const content: string[] = msg.content.slice(prefix.length).split(/ +/);
   // first index is the command
-  const command: string | number = content[0];
-  // if there is no command with name = content[0] return, else execute the command and pass msg (so I can get properties like author, etc) and args (array of strings or numbers)
+  const command: string = content[0];
+  // if there is no command with name = content[0] return, else execute the command and pass msg (so I can get properties like author, etc) and args (string[])
   if (!commands[command]) return;
   else {
     // args is anything after the command
-    const args: string[] | number[] = content.slice(1);
+    const args: string[] = content.slice(1);
     commands[command](msg, ...args);
   }
 };
--- src/commands/tenor.ts
+++ src/commands/tenor.ts
@@ -1,23 +1,35 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import dotenv from 'dotenv';
 import { Message } from 'discord.js';
 
 dotenv.config({ path: 'config.env' });
-
+// Tenor auth key
 const key: string = process.env.TENOR_KEY as string;
+/* (values: off | low | medium | high) specify the content safety filter level
+  eventually ill make a seperate command for sfw or nsfw or just let user provide safe level
+ */
+const filter: string = 'off';
 
-export const tenor = async (msg: Message, ...args: string[] | number[]) => {
+export const tenor = async (
+  msg: Message,
+  ...args: string[]
+): Promise<Message> => {
   if (args.length === 0) return msg.channel.send('Provide a keyword');
+  let keyword: string;
+  if (args.length > 1) keyword = args.join('');
+  else keyword = args[0];
+  console.log(args);
 
-  const keyword: string | number = args[0];
-
-  const url = `https://api.tenor.com/v1/search?q=${keyword}&key=${key}&contentfilter=high`;
+  const url = `https://api.tenor.com/v1/search?q=${keyword}&key=${key}&contentfilter=${filter}`;
   try {
-    const res = await axios.get(`${url}`);
+    const res: AxiosResponse = await axios.get(`${url}`);
+
+    if (res.data.results.length === 0)
+      return msg.channel.send('No results found');
 
     const index = Math.floor(Math.random() * res.data.results.length);
-    msg.channel.send(res.data.results[index].url);
+    return msg.channel.send(res.data.results[index].url);
   } catch (err) {
-    console.log(err);
+    return msg.channel.send('Error');
   }
 };
